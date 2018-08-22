843cfa6bdb494587be74eaec2b5b5009cbb49d18
PoulamiBhose <bhosepoulami@gmail.com>
2018-08-22 20:08:23 +0530

    Author: Poulami Bhose Time: 22-8-2018 08:07PM Description: Trial Expire Page Design

---
+++ app/src/main/java/intelligentsurveymanagement/activities/TrialExpireActivity.java
@@ -0,0 +1,15 @@
+package intelligentsurveymanagement.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.suman.intelligentsurveymanagement.R;
+
+public class TrialExpireActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trial_expire);
+    }
+}
